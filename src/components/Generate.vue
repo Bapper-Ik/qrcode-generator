7d49d07c6d1075cc04c8b3b4b501b6c8a535f3d4
bappa-ik <yakubumusayakubu17@gmail.com>
2022-02-06 16:52:14 +0100

    Adding download button

--- src/components/Generate.vue
+++ src/components/Generate.vue
@@ -4,19 +4,21 @@
             <input class="qrText focus:outline-none rounded-xl w-full md:w-10/12 mb-4 md:mb-0 md:mr-8 p-5" type="text" name="qrText" v-model="qrText" placeholder="type anything you want 😃">
 
             <input type="submit" value="Generate" v-on:click="generate" class="bg-blue-400 hover:bg-blue-500 text-white rounded-xl p-3 mr-0">
+            
         </form>
     </div>
     
 </template>
 
 <script>
-import '../assets/tailwind.css'
+import '../assets/tailwind.css';
+
 export default {
     name: 'Generate',
 
     data() {
         return {
-            qrText: ''  
+            qrText: '',
         }
     },
 
@@ -25,11 +27,7 @@ export default {
             e.preventDefault()
             this.$emit('qr-text', this.qrText)
             this.qrText = ''
-        }
-    },
-
-    created() {
-        
+        },  
     }
 }
 </script>
--- src/components/Output.vue
+++ src/components/Output.vue
@@ -1,13 +1,18 @@
 <template>
     <Generate v-on:qr-text="displayOutput" />
-    <div class="output mt-20 py-6 flex justify-center invisible mb-36">
-        <img id="myImg" src="" alt="">
+    <div class="output grid grid-col-2 gap-4 mt-20 py-6 flex justify-center mb-36 invisible">
+        <img class="" id="myImg" src="" alt="" />
+        <div class="bg-green-400 my-4 py-2 px-8 text-white rounded-lg">
+            <button class="" @click="onClick">Download</button>
+        </div>
+        
     </div>
 </template>
 
 
 <script>
 import Generate from "./Generate";
+import axios from "axios";
 
 export default {
     name: 'Output',
@@ -20,6 +25,26 @@ export default {
             document.querySelector('#myImg').src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrText}`
 
             document.querySelector('.output').style.visibility = 'visible'
+        },
+
+         onClick(){
+            axios({
+                url: document.querySelector('#myImg').src,
+                method: 'GET',
+                responseType: 'blob'
+
+            })
+            .then((response) => {
+                const fileurl = window.URL.createObjectURL(new Blob([response.data]))
+                const fileLink = document.createElement('a')
+                fileLink.href = fileurl
+                fileLink.setAttribute('download', 'image.jpg')
+                document.body.appendChild(fileLink)
+
+                fileLink.click();
+    
+            })
+            .catch(err => console.log(err))
         }
     }
 }
